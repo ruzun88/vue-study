e8c61a64de2d0be5e61e3cebcfbaa4efae17efb2
ruzun88 <ruzun88@gmail.com>
2021-02-02 21:16:20 +0900

    todo app 완성

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -4,14 +4,23 @@
     <span class="addContainer" v-on:click="addTodo">
       <i class="addBtn fas fa-plus" aria-hidden="true"></i>
     </span>
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">경고</h3>
+      <span slot="footer" @click="showModal = false">
+        할 일을 입력하세요.
+        <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+      </span>
+    </modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal'
 export default {
   data() {
     return {
-      newTodoItem: ''
+      newTodoItem: '',
+      showModal: false
     }
   },
   methods: {
@@ -20,6 +29,8 @@ export default {
         var value = this.newTodoItem && this.newTodoItem.trim();
         this.$emit('addTodo', value);
         this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
       }
       // console.log(this.newTodoItem);
     },
@@ -27,6 +38,9 @@ export default {
       this.newTodoItem = '';
     }
   },
+  components: {
+    Modal: Modal
+  }
 }
 </script>
 
