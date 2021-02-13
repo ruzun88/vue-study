b41e4060db5e939bb42aed3ea4166926b32e919a
ruzun88 <ruzun88@gmail.com>
2021-02-14 01:33:03 +0900

    비동기 처리 공부 완료

--- inflearn/vue-advanced/vue-news/src/views/AskView.vue
+++ inflearn/vue-advanced/vue-news/src/views/AskView.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    ask
+    <div v-for="item in ask">{{ item.title }}</div>
   </div>
 </template>
 
 <script>
-export default {
+import { fetchAskList } from '../api/index.js'
 
+export default {
+  data() {
+    return {
+      ask: []
+    }
+  },
+  created() {
+    var vm = this; // 화살표 함수를 쓰지 않으면, this가 의미하는 것이 vm이 아니므로, 별도로 빼두어야 함
+    fetchAskList()
+      .then(function(response) {
+        vm.ask = response.data;
+      })
+      .catch(error => console.log(error))
+  }
 }
 </script>
 
--- inflearn/vue-advanced/vue-news/src/views/NewsView.vue
+++ inflearn/vue-advanced/vue-news/src/views/NewsView.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    news
+    <div v-for="item in users">{{ item.title }}</div>
   </div>
 </template>
 
 <script>
-export default {
+import { fetchNewsList } from '../api/index.js'
 
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    var vm = this;
+    fetchNewsList()
+      .then(function(response) {
+        vm.users = response.data;
+      })
+      .catch(error => console.log(error))
+  }
 }
 </script>
 
