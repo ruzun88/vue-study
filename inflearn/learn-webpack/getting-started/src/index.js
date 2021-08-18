dbb464efb236cc8c2d1ae94caf7677b8ddd462bd
yongjun <ruzun88@gmail.com>
2021-08-18 23:10:04 +0900

    웹팩 시작하기 끝

--- inflearn/learn-webpack/getting-started/src/index.js
+++ inflearn/learn-webpack/getting-started/src/index.js
@@ -1,8 +1,14 @@
+// ES6
+import _ from 'lodash'
+
+// ES5
+// var _ = require('lodash')
+
 function component() {
-  var element = document.createElement("div");
+  var element = document.createElement('div');
 
   /* lodash is required for the next line to work */
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
